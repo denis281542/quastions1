4f4587ff2e9f0921b593550919e40ceed6081f22
denis281542 <deniskuimov@mail.ru>
2019-12-13 22:58:22 +0500

    Added test

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,12 @@
 class AnswersController < ApplicationController
+  def creat
+    @post = Post.find(params[:post_id])
+    @answer = @post.answers.create(answer_params)
+    redirect_to post_path(@post)
+  end
+ 
+  private
+    def comment_params
+      params.require(:answer).permit( :body)
+    end
 end
